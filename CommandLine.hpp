f14fc968bd4f86bc6048f31e38c764261a2e15e3
Thomas E. Vaughan <tevaughan@gmail.com>
2014-09-04 15:31:50 -0600

    Added license and copyright information.

--- CommandLine.hpp
+++ CommandLine.hpp
@@ -1,4 +1,21 @@
 
+// Copyright 2014  Thomas E. Vaughan
+//
+// This file is part of Ulam.
+//
+// Ulam is free software: You can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either Version 3 of the License, or (at your option) any later
+// version.
+//
+// Ulam is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Ulam.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef COMMAND_LINE_HPP
 #define COMMAND_LINE_HPP
 
